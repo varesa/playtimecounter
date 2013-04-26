a4ed614dc53f53d93d7d352ecb7ca54a0ef1f458
Esa Varemo <esa@kuivanto.fi>
2013-04-26 21:34:38 +0300

    Added the script

---
+++ laske_ajat.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+
+import time
+import math
+
+print("Counting time")
+
+f = open('peliajat')
+lines = [line.strip() for line in f.readlines()]
+f.close()
+
+state     = ""
+totaltime = 0
+
+linetime  = 0
+oldtime   = 0
+
+for line in lines:
+
+	newtime = time.strptime(line[:19], "%Y-%m-%d %H:%M:%S")
+	#print(line[:19])
+
+	if "connecting" in line:
+		print("connecting")
+		state = "c"
+	
+	elif "lost con" in line:
+		print("disconnect")
+		if state is "c" and oldtime > 0:
+			#print("start: " + str(time.mktime(oldtime)))
+			#print("stop : " + str(time.mktime(newtime)))
+			timespent = time.mktime(newtime) - time.mktime(oldtime)
+			
+			h = math.floor(timespent / 3600)
+			m = math.floor((timespent - h*3600) / 60)
+			s = timespent - h*3600 - m*60
+			
+			#print("secs : " + str(timespent))
+			print("h: " + str(h) + ", m: " + str(m) + ", s: " + str(s))
+
+			totaltime += timespent
+
+		
+		state = "d"
+	oldtime = newtime
+
+
+h = math.floor(totaltime / 3600)
+m = math.floor((totaltime - h*3600) / 60)
+s = totaltime - h*3600 - m*60
+
+print("Total:\nh: " + str(h) + ", m: " + str(m) + ", s: " + str(s))
